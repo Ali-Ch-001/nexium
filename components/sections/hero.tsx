f67809852b4befce767b9ba8577e7ca4dbd9c80b
Ali Mohsin <108975862+Ali-Ch-001@users.noreply.github.com>
2025-01-26 14:26:16 +0500

    fix space

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -21,8 +21,7 @@ export function Hero() {
     <div className="flex flex-col items-center justify-center h-[40rem]">
 
         <div className=" text-neutral-300 dark:text-neutral-400" style={{ fontSize: '4rem' }} >
-            Build
-            <FlipWords words={words} />Products
+            Build<FlipWords words={words} />Products
         </div>
 
       <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 mt-10">
