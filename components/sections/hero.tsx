08730a4647b1bccdac68deda25f324fbae8d3780
Ali Mohsin <108975862+Ali-Ch-001@users.noreply.github.com>
2025-01-26 14:34:52 +0500

    mobile responsive

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -18,11 +18,10 @@ export function Hero() {
 
 
   return (
-    <div className="flex flex-col items-center justify-center h-[40rem]">
-
-        <div className=" text-neutral-300 dark:text-neutral-400" style={{ fontSize: '4rem' }} >
-            Build<FlipWords words={words} />Products
-        </div>
+<div className="flex flex-col text-center items-center justify-center h-auto md:h-[40rem]">
+      <div className="text-neutral-300 dark:text-neutral-400 text-5xl md:text-6xl mt-52 md:mt-48">
+          Build<FlipWords words={words} />Products
+      </div>
 
       <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 mt-10">
         <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
