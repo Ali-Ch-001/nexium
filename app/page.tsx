f159f3e64a6ed19c4a4f9e2b34f13c821949ec28
Ali Mohsin <108975862+Ali-Ch-001@users.noreply.github.com>
2025-02-09 01:52:41 +0500

    fixessss

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import NavbarMenu from "@/components/ui/navbar-menu";
+import NavbarMenu from "@/components/sections/navbar-menu";
 import { Hero } from "@/components/sections/hero";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <NavbarMenu />
       <Hero />
-    </div>
+    </>
   );
 }
--- components/sections/navbar-menu.tsx
+++ components/sections/navbar-menu.tsx
@@ -2,7 +2,7 @@
 import { motion } from "framer-motion";
 import Link from "next/link";
 import Image from "next/image";
-import { HoveredLink } from "./HoveredLink";
+import { HoveredLink } from "../ui/HoveredLink";
 
 const transition = {
   type: "spring",
